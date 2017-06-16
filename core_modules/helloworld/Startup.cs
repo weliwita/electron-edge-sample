b12d34a35a5ff5b7233480ccdcb0da7c6ea552bb
Rasika Weliwita <rasika.weliwita@cslworld.com>
2017-06-16 14:36:09 +0530

    working with new xproj format - finaly get it working

--- core_modules/helloworld/Startup.cs
+++ core_modules/helloworld/Startup.cs
@@ -2,16 +2,16 @@
 {
     using System.Threading.Tasks;
     using System;
-//     using System.Xml;
-//    using  System.Security.Cryptography.Xml;
+    using System.Xml;
+    using  System.Security.Cryptography.Xml;
     public class Startup
     {
         public async Task<object> Invoke(object input)
         {
-        //    var doc = new System.Xml.XmlDocument();
-        //    doc.LoadXml("<a></a>");
+           var doc = new System.Xml.XmlDocument();
+           doc.LoadXml("<a></a>");
 
-        //   var signedXml = new System.Security.Cryptography.Xml.SignedXml();
+        var signedXml = new System.Security.Cryptography.Xml.SignedXml();
         var time = DateTime.Now;
           var x= "";
             return "Hello from dot net core" + time;
